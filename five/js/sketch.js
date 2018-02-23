f4f59b905aaed3b2f1cd3e863de7151c4e33c0a2
JialinZhai <jz2893@columbia.edu>
2018-02-23 02:15:09 -0500

    Initial commit

--- five/js/sketch.js
+++ five/js/sketch.js
@@ -1,41 +1,59 @@
-var leftX = 120;
-var rightX = 620;
-var bottomY = 500;
+var leftX = 320;
+var rightX = 820;
+var bottomY = 600;
 var topY = 100;
 var percentage = ["100%", "80%", "60%", "40%", "20%", "0"];
 var district = ["West Harlem", "Central Harlem", "East Harlem"];
 function setup() {
-  createCanvas(800, 800);
+  createCanvas(1000, 1000);
   textFont('Roboto');
-  // background(100);
   textAlign(RIGHT, CENTER);
   noLoop();
 }
 function draw() {
+
   for (var i = 0; i < percentage.length; i++) {
     textSize(15);
-    fill(0, 102, 153);
-    text(percentage[i], 100, 100 + 80 * i);
-    stroke(214, 214, 194);
+    fill(0);
+    text(percentage[i], 300, 100 + 80 * i);
+
     line (leftX, 100 + 80 * i, rightX, 100 + 80 * i);
   }
 
-  line (leftX, bottomY, rightX, bottomY);
-  text('West Harlem', 215, 80);
-  text('Central Harlem', 390, 80);
-  text('East Harlem', 580, 80);
-  text('Total Buildings', 715, 530);
-  text('2026', 190, 530);
-  text('4645', 360, 530);
-  text('2866', 560, 530);
 
-  var startingY1 = topY;
+  fill(102, 102, 255);
+
+  rect (360, 570, 10, 10);
+  fill(128, 170, 255);
+  rect (560, 570, 10, 10);
+  fill(159, 223, 159);
+  rect (360, 590, 10, 10);
+  fill(255, 173, 51);
+  rect (560, 590, 10, 10);
+  fill(214, 214, 194);
+  rect (360, 610, 10, 10);
+  fill(0, 102, 153);
+
   fill(0);
-  stroke(255);
+  text('West Harlem', 415, 80);
+  text('Central Harlem', 590, 80);
+  text('East Harlem', 780, 80);
+  text('Total Buildings', 915, 530);
+  text('2026', 390, 530);
+  text('4645', 560, 530);
+  text('2866', 760, 530);
+  textAlign(LEFT, CENTER);
+  text('Multi-Family Walk Up', 390, 576);
+  text('Multi-Family Elevator', 590, 576);
+  text('One & Two Family', 390, 597);
+  text('Mixed Residential & Commertial', 590, 596);
+  text('Other Uses', 390, 616);
+
+  var startingY1;
+  startingY1 = topY;
   for (var i = 0; i < 5; i++) {
-    var number1 = [0.1619, 0.1194, 0.1851, 0.1022, 0.4314];
     if (i == 0){
-     fill (214, 214, 194);
+    fill (214, 214, 194);
     }
     else if (i == 1){
     fill (255, 173, 51);
@@ -49,15 +67,16 @@ function draw() {
     else{
     fill(102, 102, 255);
     }
+    stroke(0);
+    var number1 = [0.1619, 0.1194, 0.1851, 0.1022, 0.4314];
     rect(leftX, startingY1, 106, map(number1[i], 0, 1, 0, 400));
     startingY1 = startingY1 + map(number1[i], 0, 1, 0, 400);
   }
-
-  var startingY2 = topY;
+  var startingY2;
+  startingY2 = topY;
   for (var i = 0; i < 5; i++) {
-    var number2 = [0.1729, 0.1548, 0.1255, 0.0659,0.4809];
     if (i == 0){
-     fill (214, 214, 194);
+    fill (214, 214, 194);
     }
     else if (i == 1){
     fill (255, 173, 51);
@@ -71,16 +90,16 @@ function draw() {
     else{
     fill(102, 102, 255);
     }
-      rect(leftX + 106, startingY2, 244, map(number2[i], 0, 1, 0, 400));
-    print(map(number2[i], 0, 1, 0, 400));
+    stroke(0);
+    var number2 = [0.1729, 0.1548, 0.1255, 0.0659,0.4809];
+    rect(leftX + 106, startingY2, 244, map(number2[i], 0, 1, 0, 400));
     startingY2 = startingY2 + map(number2[i], 0, 1, 0, 400);
   }
-
-  var startingY3 = topY;
+  var startingY3;
+  startingY3 = topY;
   for (var i = 0; i < 5; i++) {
-    var number3 = [0.3231, 0.2701, 0.0290, 0.0691, 0.3087];
     if (i == 0){
-     fill (214, 214, 194);
+    fill (214, 214, 194);
     }
     else if (i == 1){
     fill (255, 173, 51);
@@ -94,9 +113,9 @@ function draw() {
     else{
     fill(102, 102, 255);
     }
+    stroke(0);
+    var number3 = [0.3231, 0.2701, 0.0290, 0.0691, 0.3087];
     rect(leftX + 350, startingY3, 150, map(number3[i], 0, 1, 0, 400));
-    print(map(number3[i], 0, 1, 0, 400));
     startingY3 = startingY3 + map(number3[i], 0, 1, 0, 400);
   }
-
 }
